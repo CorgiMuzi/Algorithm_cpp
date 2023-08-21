102cadde0c638df6a34e604e4b46ffd8aac804ab
CorgiMuzi <kjhcorgi99@gmail.com>
2023-08-21 17:19:06 +0900

    + Specific shortest path_1504 solved

--- CodingTest/CodingTest/SpecificShortestPath_1504.cpp
+++ CodingTest/CodingTest/SpecificShortestPath_1504.cpp
@@ -1,25 +1,76 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+
+#define INF 2400000
 
 using namespace std;
 
-int vertexNum = 0, edgeNum = 0;
-vector<vector<pair<int, int>>> distances(801);
+typedef pair<int, int> pii;
 
-void travel() {
+int N = 0, E = 0;
+vector<pii> edges[801];
+int elapsedDist[801];
 
+void dijkstra(int beg) {
+	for (int i = 1; i <= N; ++i) {
+		elapsedDist[i] = INF;
+	}
+	elapsedDist[beg] = 0;
+
+	priority_queue<pii> trail;
+	trail.push(make_pair(0, beg));
+
+	while (!trail.empty()) {
+		int curDist = -trail.top().first;
+		int curNode = trail.top().second;
+		trail.pop();
+
+		if (elapsedDist[curNode] < curDist) continue;
+
+		for (vector<pii>::size_type i = 0; i < edges[curNode].size(); ++i) {
+			int nxtDist = edges[curNode][i].first + curDist;
+			int nxtNode = edges[curNode][i].second;
+
+			if (nxtDist < elapsedDist[nxtNode]) {
+				elapsedDist[nxtNode] = nxtDist;
+				trail.push(make_pair(-elapsedDist[nxtNode], nxtNode));
+			}
+		}
+	}
 }
 
 int main() {
-	cin >> vertexNum >> edgeNum;
+	cin >> N >> E;
 
 	int beg = 0, end = 0, dist = 0;
-	for (int i = 0; i < vertexNum; ++i) {
+	for (int i = 0; i < E; ++i) {
 		cin >> beg >> end >> dist;
-		distances[beg].push_back(make_pair(end, dist));
-		distances[end].push_back(make_pair(beg, dist));
+
+		edges[beg].push_back(make_pair(dist, end));
+		edges[end].push_back(make_pair(dist, beg));
 	}
 
-	int v1 = 0, v2 = 0;
-	cin >> v1 >> v2;
+	int u = 0, v = 0;
+	cin >> u >> v;
+
+	int begToU = 0, begToV = 0;
+	dijkstra(1);
+	begToU = elapsedDist[u];
+	begToV = elapsedDist[v];
+
+	int uToV = 0, uToN = 0;
+	dijkstra(u);
+	uToV = elapsedDist[v];
+	uToN = elapsedDist[N];
+
+	int vToN = 0;
+	dijkstra(v);
+	vToN = elapsedDist[N];
+
+	int ans = INF;
+	ans = min(ans, begToU + uToV + vToN);
+	ans = min(ans, begToV + uToV + uToN);
+
+	cout << (ans >= INF ? -1 : ans);
 }
