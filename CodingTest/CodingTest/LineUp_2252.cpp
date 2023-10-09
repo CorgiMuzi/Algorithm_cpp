4a40a80bbb909ccb251163be777b43542354693d
CorgiMuzi <kjhcorgi99@gmail.com>
2023-10-09 10:11:39 +0900

    + Line up_2252 solved

---
+++ CodingTest/CodingTest/LineUp_2252.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int N = 0, M = 0;
+vector<int> inputs[32001];
+int lineParent[32001];
+
+int main() {
+	cin >> N >> M;
+
+	int A = 0, B = 0;
+	for (int i = 0; i < M; ++i) {
+		cin >> A >> B;
+		
+	}
+}
