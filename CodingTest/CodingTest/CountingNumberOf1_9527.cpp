26cd6ed4f9c09e2f7392c505d9aa331b6a6699e4
CorgiMuzi <kjhcorgi99@gmail.com>
2023-11-18 20:42:53 +0900

    + Jerray and tom_16430 solved  + Counting number of 1_9527 added

---
+++ CodingTest/CodingTest/CountingNumberOf1_9527.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+long long A = 0, B = 0;
+
+int main() {
+	cin >> A >> B;
+
+}
---
+++ CodingTest/CodingTest/JerryAndTom_16430.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int numerator, denominator;
+	cin >> numerator >> denominator;
+
+	cout << denominator - numerator << " " << denominator;
+}
