07b3eb97e0292ef13b854a285eb5c0616f132f24
CorgiMuzi <kjhcorgi99@gmail.com>
2023-11-23 17:10:59 +0900

    + Counting number of 1_9527 solved   + Add new filter "NeedToRemind"   + Edit ReadMe file     - Add definitions for each filters

--- CodingTest/CodingTest/CountingNumberOf1_9527.cpp
+++ CodingTest/CodingTest/CountingNumberOf1_9527.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-long long A = 0, B = 0;
+typedef long long lld;
+
+lld A = 0, B = 0, C = 0;
+lld one[55];
+
+lld countNumber(lld arg) {
+	lld ret = arg & 1;
+	for (int i = 54; i > 0; --i) {
+		if (arg & (1LL << i)) {
+			ret += one[i - 1] + (arg - (1LL << i) + 1);
+			arg -= 1LL << i;
+		}
+	}
+
+	return ret;a
+}
 
 int main() {
 	cin >> A >> B;
 
+	one[0] = 1;
+	for (int i = 1; i < 55; ++i) {
+		one[i] = 2 * one[i - 1] + (1LL << i);
+	}
+	
+	cout << countNumber(B) - countNumber(A - 1);
 }
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # History of learning algorithm with coding test
 일일 1문제 목표<br>
 꾸준히는 아니더라도 풀이 과정을 Notion에 기록
+
+Filter list
+- Deprecated : 해결된 문제지만 비효율적인 풀이 방법으로 작성되었음
+- NeedToRemind : 해결되었지만 스스로 해결하지 못한 방법으로 작성되었음
+- Solved : 해결됨
