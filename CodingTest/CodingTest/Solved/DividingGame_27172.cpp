2127246359fbdca7d284af270dc24ca871792d1c
CorgiMuzi <kjhcorgi99@gmail.com>
2023-12-18 10:30:13 +0900

    + Dividing game_27172 solved

---
+++ CodingTest/CodingTest/Solved/DividingGame_27172.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> players;
+bool cards[1000001];
+int score[1000001];
+int N;
+
+int main() {
+	cin >> N;
+	
+	fill_n(score, 1000001, 0);
+	for(int i = 0; i < N; ++i) {
+		int card; cin >> card;
+		players.push_back(card);
+		cards[card] = true;
+	}
+	
+	for(const int& card : players) {
+		for(int i = 2 * card; i < 1000001; i += card) {
+			if(cards[i]) {
+				score[i]--;
+				score[card]++;
+			}
+		}
+	}
+
+	for(const int& card : players) {
+		cout << score[card] << " ";
+	}
+}
