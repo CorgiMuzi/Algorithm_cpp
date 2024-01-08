ec7d0c4fbdd7738a8b54cfd69f4cde5ac70aebf5
CorgiMuzi <kjhcorgi99@gmail.com>
2024-01-08 13:19:35 +0900

    + Square no no number_1016 solved + Bracket string_1023 added

---
+++ CodingTest/CodingTest/BracketString_1023.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long lld;
+
+int main() {
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+	
+	int N, K;
+	cin >> N >> K;
+	
+}
---
+++ CodingTest/CodingTest/Solved/SquareNoNoNumber_1016.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long lld;
+
+bool sieve[1000001];
+lld lower, upper;
+
+int main() {
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	cin >> lower >> upper;
+
+	for(lld i = 2; i * i <= upper; ++i) {
+		lld square = i * i;
+		lld n = lower / square;
+		if(lower % (i * i)) n++;
+		for(lld j = n; j * square <= upper; ++j) {
+			sieve[j * square - lower] = true;
+			n++;
+		}
+	}
+
+	lld ans = count(sieve, sieve + (upper - lower) + 1, false);
+	cout << ans;
+}
