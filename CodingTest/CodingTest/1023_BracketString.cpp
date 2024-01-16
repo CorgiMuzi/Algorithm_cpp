b8e9224bf1b3e018b91a586302e91ea78f6c850d
CorgiMuzi <kjhcorgi99@gmail.com>
2024-01-16 09:22:13 +0900

    + 1049_Guitar strings solved

--- CodingTest/CodingTest/1023_BracketString.cpp
+++ CodingTest/CodingTest/1023_BracketString.cpp
@@ -3,18 +3,40 @@
 using namespace std;
 
 typedef long long lld;
-// nbstr[a][b] = a 길이의 문자열이 b 값을 가질 때 괄호 ㄴㄴ 문자열의 갯수
-int nbstr[51][101];
+
+lld nbstr[51][101];
+
 int N;
 lld K;
+
+string solve();
 
 int main() {
 	cin.tie(nullptr);
 	ios_base::sync_with_stdio(false);
 
 	cin >> N >> K;
-	nbstr[0][50] = 1;
-	for(int i = 1; i <= N; ++i) {
-		
+
+	fill_n(&nbstr[0][0], 101, 1);
+	nbstr[0][50] = 0;
+	
+	for(int r = 1; r <= N; ++r) {
+		fill_n(&nbstr[r][0], 50, pow(2, r));
 	}
+
+	for(int r = 1; r <= N; ++r) {
+		for(int c = 50; c <= 100; ++c) {
+			nbstr[r][c] = nbstr[r - 1][c - 1] + nbstr[r - 1][c + 1];
+		}
+	}
+
+	cout << solve();
+}
+
+string solve() {
+	if(nbstr[N][50] > K) return "-1";
+
+
+	
+	//return ret; 
 }
---
+++ CodingTest/CodingTest/BOJ/1049_GuitarStrings.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+typedef pair<int, int> pii;
+
+int N, M;
+pii brand[51];
+int cost;
+
+bool Package(const pii& left, const pii& right);
+bool Single(const pii& left, const pii& right);
+
+int main() {
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	cin >> N >> M;
+	for(int i = 0; i < M; ++i) {
+		cin >> brand[i].first >> brand[i].second;
+	}
+
+	sort(brand, brand + M, Package);
+	int remainders = N % 6;
+	int coin = (N / 6) * brand[0].first;
+	if(remainders > 0) coin += brand[0].first;
+
+	cost = coin;
+	coin -= brand[0].first;
+	
+	sort(brand, brand + M, Single);
+
+	if(remainders > 0) {
+		coin += (remainders * brand[0].second);
+
+		cost = min(cost, coin);
+	}
+
+	cost = min(cost, N * brand[0].second);
+
+	cout << cost;
+}
+
+bool Package(const pii& left, const pii& right) {
+	return left.first < right.first;
+}
+
+bool Single(const pii& left, const pii& right) {
+	return left.second < right.second;
+}
