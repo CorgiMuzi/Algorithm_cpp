0a150e1c2539bddc54e3fb8a4b718574ace24788
CorgiMuzi <kjhcorgi99@gmail.com>
2024-01-18 09:49:56 +0900

    + 1052_Water bottle solved + 1135_Report the News added

---
+++ CodingTest/CodingTest/1135_ReportTheNews.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+int boss[51];
+
+int main() {
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	cin >> N;
+
+	fill_n(boss, N, 0);
+	for(int i = 1; i < N; ++i) {
+		cin >> boss[i];
+	}
+}
---
+++ CodingTest/CodingTest/BOJ/1052_WaterBottle.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int N, K;
+int a, b = 0;
+
+int CountBits(int num);
+
+int main() {
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	cin >> N >> K;
+
+	int a = CountBits(N);
+	while(a > K) {
+		b++;
+		N++;
+		a = CountBits(N);
+	}
+
+	cout << b;
+}
+
+int CountBits(int num) {
+	int ret = 0;
+	while(num > 0) {
+		if(num % 2 == 1) ret++;
+		num /= 2;
+	}
+
+	return ret;
+}
