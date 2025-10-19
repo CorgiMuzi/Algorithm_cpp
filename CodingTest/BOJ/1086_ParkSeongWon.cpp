c57e18c01a3f525920abe01b095d1a86a1d2e25d
CorgiMuzi <49934162+CorgiMuzi@users.noreply.github.com>
2025-10-19 20:14:11 +0900

    + 1086_ParkSeongWon solved

---
+++ CodingTest/BOJ/1086_ParkSeongWon.cpp
@@ -0,0 +1,80 @@
+﻿
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<vector<long long>> group(1 << 15, vector<long long>(100, 0));
+vector<long long> factorial(16);
+vector<int> remainders(15);
+vector<int> ten_pow(51);
+
+long long GCD(long long n1, long long n2)
+{
+    if (n2 == 0) return n1;
+    return GCD(n2, n1 % n2);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    freopen("input.txt", "r", stdin);
+
+    int N;
+    cin >> N;
+    vector<string> input(N);
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> input[i];
+    }
+    int K;
+    cin >> K;
+
+    ten_pow[0] = 1;
+    factorial[0] = 1;
+    for (int i = 1; i <= 50; ++i) ten_pow[i] = (ten_pow[i - 1] * 10) % K;
+    for (int i = 1; i <= N; ++i) factorial[i] = factorial[i - 1] * i;
+
+    for (int i = 0; i < N; ++i)
+    {
+        int rem = 0;
+        for (char c : input[i])
+        {
+            int ic = c - '0';
+            rem = (rem * 10 + ic) % K;
+        }
+        remainders[i] = rem;
+    }
+
+    group[0][0] = 1;
+    for (int i = 0; i < (1 << N); ++i)
+    {
+        for (int rem = 0; rem < K; ++rem)
+        {
+            if (group[i][rem] == 0) continue;
+
+            for (int j = 0; j < N; ++j)
+            {
+                if (i & (1 << j)) continue;
+                int new_rem = (rem * ten_pow[input[j].length()] + remainders[j]) % K;
+                group[i | (1 << j)][new_rem] += group[i][rem];
+            }
+        }
+    }
+
+    long long result = group[(1 << N) - 1][0];
+    long long total = factorial[N];
+    
+    long long gcd = GCD(total, result);
+
+    result /= gcd;
+    total /= gcd;
+
+    cout << result << "/" << total << "\n";
+
+    return 0;
+}
---
+++ CodingTest/BOJ/2243_CandyJar.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void UpdateTree(int taste, int num, vector<long long>& tree, int node, int left, int right)
+{
+    if (taste < left || taste > right) return;
+    if (left == right)
+    {
+        tree[node] += num;
+        return;
+    }
+    
+    int mid = (left + right) / 2;
+    UpdateTree(taste, num, tree, node * 2, left, mid);
+    UpdateTree(taste, num, tree, node * 2 + 1, mid + 1, right);
+
+    tree[node] = tree[node * 2] + tree[node * 2 + 1];
+}
+
+int QueryTree(int rank, vector<long long>& tree, int node, int left, int right)
+{
+    if (left == right) return left;
+    int mid = (left + right) / 2;
+    if (rank <= tree[node * 2]) return QueryTree(rank, tree, node * 2, left, mid);
+    return QueryTree(rank - tree[node * 2], tree, node * 2 + 1, mid + 1, right);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    freopen("input.txt", "r", stdin);
+
+    const int Max = 1'000'000;
+    
+    int N; cin >> N;
+    vector<long long> tree(4 * Max);
+    for (int i = 0; i < N; ++i)
+    {
+        int a, b; cin >> a >> b;
+        if (a == 1)
+        {
+            int c = QueryTree(b, tree, 1, 1, Max);
+            UpdateTree(c, -1, tree, 1, 1, Max);
+            cout << c << "\n";
+        }else if (a == 2)
+        {
+            int c; cin >> c;
+            UpdateTree(b, c, tree, 1, 1, Max);
+        }
+    }
+    return 0;
+}
