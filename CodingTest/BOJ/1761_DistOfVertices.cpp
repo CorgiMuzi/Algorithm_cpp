5f8b1c7d1aa9fd8a49cd4ab02855b4cfd2f325ba
CorgiMuzi <49934162+CorgiMuzi@users.noreply.github.com>
2025-10-26 13:07:15 +0900

    + 1761_DistOfVertices solved
    + 1786_Search solved

---
+++ CodingTest/BOJ/1761_DistOfVertices.cpp
@@ -0,0 +1,107 @@
+﻿#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+typedef pair<int, int> pii;
+
+const int MaxJump = 16;
+
+vector<vector<pii>> vertices(40001);
+vector<vector<int>> parent(40001, vector<int>(MaxJump, 0));
+vector<int> depth(40001);
+vector<int> root_distance(40001);
+vector<bool> visited(40001, false);
+
+
+
+int GetCommonParentVertex(int v1, int v2)
+{
+    if (depth[v1] < depth[v2]) swap(v1, v2);
+
+    for (int i = MaxJump - 1; i >= 0; --i)
+    {
+        if (depth[v1] - (1 << i) >= depth[v2]) v1 = parent[v1][i];
+    }
+
+    if (v1 == v2) return v1;
+
+    for (int i = MaxJump - 1; i >= 0; --i)
+    {
+        if (parent[v1][i] != parent[v2][i])
+        {
+            v1 = parent[v1][i];
+            v2 = parent[v2][i];
+        }
+    }
+
+    return parent[v1][0];
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    freopen("input.txt", "r", stdin);
+
+    int N; cin >> N;
+    
+    for (int i = 0; i < N - 1; ++i)
+    {
+        int v1, v2, d;
+        cin >> v1 >> v2 >> d;
+        vertices[v1].emplace_back(v2, d);
+        vertices[v2].emplace_back(v1, d);
+    }
+
+    for (int i = 0; i < N; ++i) parent[i][0] = i;
+
+    stack<int> stk;
+    stk.push(1);
+    visited[1] = true;
+    root_distance[1] = 0;
+
+    while (!stk.empty())
+    {
+        int cur = stk.top();
+        bool isNxtExist = false;
+
+        for (const pii& nxt : vertices[cur])
+        {
+            if (visited[nxt.first]) continue;
+            stk.push(nxt.first);
+            visited[nxt.first] = true;
+
+            parent[nxt.first][0] = cur;
+            depth[nxt.first] = depth[cur] + 1;
+            root_distance[nxt.first] = root_distance[cur] + nxt.second;
+            
+            isNxtExist = true;
+            break;
+        }
+
+        if (!isNxtExist)
+        {
+            stk.pop();
+        }
+    }
+
+    for (int j = 1; j < MaxJump; ++j)
+    {
+        for (int i = 1; i <= N; ++i)
+        {
+            parent[i][j] = parent[parent[i][j - 1]][j - 1];
+        }
+    }
+    
+    int M; cin >> M;
+    for (int i = 0; i < M; ++i)
+    {
+        int v1, v2; cin >> v1 >> v2;
+        int common_root = GetCommonParentVertex(v1, v2);
+
+        cout << root_distance[v1] + root_distance[v2] - 2 * root_distance[common_root] << "\n";
+    }
+}
---
+++ CodingTest/BOJ/1786_Search.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> pattern(1000001);
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    freopen("input.txt", "r", stdin);
+
+    string T, P;
+    getline(cin, T);
+    getline(cin, P);
+
+    pattern[0] = 0;
+    
+    int j = 0;
+    for (int i = 1; i < P.length(); ++i)
+    {
+        while (j > 0 && P[i] != P[j])
+        {
+            j = pattern[j - 1];
+        }
+
+        if (P[i] == P[j]) j++;
+        pattern[i] = j;
+    }
+
+    int t = 0;
+    int p = 0;
+
+    vector<int> find_pos;
+
+    while (t < T.length())
+    {
+        if (T[t] == P[p])
+        {
+            t++;
+            p++;
+        }
+
+        if (p == P.length())
+        {
+            find_pos.push_back(t - p + 1);
+            p = pattern[p - 1];
+        }else if (T[t] != P[p])
+        {
+            if (p > 0) p = pattern[p - 1];
+            else t++;
+        }
+    }
+
+    cout << find_pos.size() << "\n";
+    for (int pos : find_pos) cout << pos << " ";
+
+    return 0;
+}
